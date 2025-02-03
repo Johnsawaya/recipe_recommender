c54fe5a737ed69d52f225a89705096b65f352422
Johnsawaya <sawayajohn@Hotmail.com>
2025-02-03 21:02:27 +0200

    frontend

---
+++ lib/card_widget.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class card_widget extends StatelessWidget {
+  final String title;
+  final String calories;
+  final String protein;
+  final String prep_time;
+  final String image_url;
+  card_widget({
+    required this.title,
+    required this.calories,
+    required this.protein,
+    required this.prep_time,
+    required this.image_url,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+      width: MediaQuery.of(context).size.width,
+      height: 180,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.6),
+            offset: Offset(
+              0.0,
+              10.0,
+            ),
+            blurRadius: 10.0,
+            spreadRadius: -6.0,
+          ),
+        ],
+        image: DecorationImage(
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.35),
+            BlendMode.multiply,
+          ),
+          image: NetworkImage(image_url),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Stack(
+        children: [
+          Align(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 19,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            alignment: Alignment.center,
+          ),
+          Align(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.favorite_border,
+                        color: Colors.yellow,
+                        size: 18,
+                      ),
+                      SizedBox(width: 7),
+                      Text(calories),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.schedule,
+                        color: Colors.yellow,
+                        size: 18,
+                      ),
+                      SizedBox(width: 7),
+                      Text(prep_time),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            alignment: Alignment.bottomLeft,
+          ),
+        ],
+      ),
+    );
+  }
+}
