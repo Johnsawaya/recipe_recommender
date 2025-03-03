da47b4a3eec4fec15e2a975fd7548b7d86b9a27c
Johnsawaya <sawayajohn@Hotmail.com>
2025-03-03 10:24:55 +0200

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Use environment variable
+  connectionString: process.env.dpg-cv2m8oan91rc73bvc3og-a, // Use environment variable
   ssl: {
     rejectUnauthorized: false, // Required for Render's managed PostgreSQL
   },
