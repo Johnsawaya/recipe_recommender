2c95cfff56df7025fa5ac18218d54278823ae8d6
Johnsawaya <sawayajohn@Hotmail.com>
2025-03-03 10:35:16 +0200

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const { Pool } = require("pg");
-
+DATABASE_URL=postgres://recipe_recommendationdb_zsvn_user:2UhvPlyf1rTCzkyVrA2oCAEhwl22ZMru@dpg-cv2m8oan91rc73bvc3og-a:5432/recipe_recommendationdb_zsvn;
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL=postgres://recipe_recommendationdb_zsvn_user:2UhvPlyf1rTCzkyVrA2oCAEhwl22ZMru@dpg-cv2m8oan91rc73bvc3og-a:5432/recipe_recommendationdb_zsvn
+  connectionString: process.env.DATABASE_URL
 , // Use environment variable
   ssl: {
     rejectUnauthorized: false, // Required for Render's managed PostgreSQL
